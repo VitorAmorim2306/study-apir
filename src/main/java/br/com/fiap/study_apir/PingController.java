8e8bde4bcdef9f050ae020dfe70ef319a42d2113
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-02 22:51:28 -0300

    feature: Add ping controller

---
+++ src/main/java/br/com/fiap/study_apir/PingController.java
@@ -0,0 +1,15 @@
+package br.com.fiap.study_apir;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping 
+public class PingController {
+
+    @GetMapping ("/palmeiras")
+    public String ping() {
+        return "NUGGETS";
+    }
+}
